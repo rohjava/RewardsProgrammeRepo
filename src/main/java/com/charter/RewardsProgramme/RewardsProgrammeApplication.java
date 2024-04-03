3f407b8b62f6f1c9a16961d9ea5edc17a61bfc88
rohjava <rohit.govindgari@gmail.com>
2024-04-03 11:20:01 -0400

    New Programme for calculating the rewards

---
+++ src/main/java/com/charter/RewardsProgramme/RewardsProgrammeApplication.java
@@ -0,0 +1,13 @@
+package com.charter.RewardsProgramme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsProgrammeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsProgrammeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charter/RewardsProgramme/RewardsProgrammeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charter.RewardsProgramme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsProgrammeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
